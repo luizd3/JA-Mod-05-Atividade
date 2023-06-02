d4466ed567de9c7bc017353adab3b7ca529930bc
Luiz Dorça <luizdorca@gmail.com>
2023-06-02 16:26:12 -0300

    Inicial commit

---
+++ src/main/java/com/ld/mod05atividade/Mod05atividadeApplication.java
@@ -0,0 +1,13 @@
+package com.ld.mod05atividade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mod05atividadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mod05atividadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ld/mod05atividade/Mod05atividadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld.mod05atividade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod05atividadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
